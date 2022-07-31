18e79766504237a9794713c71c633126744ebf54
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-07-31 12:51:02 +0530

    random commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from './components/Footer';
 function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
